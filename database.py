eb8a1896fae43ff0bdc354d56a01fcef77ad6fd3
Phyo Han Kyaw <phyohankyaw86@gmail.com>
2026-06-28 15:17:37 +0630

    fix db conn

--- database.py
+++ database.py
@@ -4,10 +4,10 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_conn():
-    return psycopg2.connect(DATABASE_URL)
+    url = os.environ.get("DATABASE_URL")
+    return psycopg2.connect(url)
 
 def init_db():
     conn = get_conn()
